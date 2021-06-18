b1583ca494f03ee9a2549edcbd7e6e6e7604d2d0
Ryan Won <ryanwon7@gmail.com>
2021-06-18 04:33:54 -0400

    finished initial demo

--- src/Demo.js
+++ src/Demo.js
@@ -1,42 +1,35 @@
 import React, { Suspense, useRef } from 'react';
-//import * as THREE from 'three';
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { Canvas, useLoader } from '@react-three/fiber'
-//import Ring from './Ring'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber'
+import Ring from './Ring'
 
-function ArWing() {
-	const group = useRef()
-  const { nodes } = useLoader('/ring.gltf')
-  return (
-    <group ref={group}>
-      <mesh geometry={nodes.mesh_0.geometry} material={nodes.mesh_0.material} />
-      <mesh geometry={nodes.mesh_1.geometry} material={nodes.mesh_1.material} />
-      <mesh geometry={nodes.mesh_2.geometry} material={nodes.mesh_2.material} />
-      <mesh geometry={nodes.mesh_3.geometry} material={nodes.mesh_3.material} />
-      <mesh geometry={nodes.mesh_4.geometry} material={nodes.mesh_4.material} />
-      <mesh geometry={nodes.mesh_5.geometry} material={nodes.mesh_5.material} />
-      <mesh geometry={nodes.mesh_6.geometry} material={nodes.mesh_6.material} />
-      <mesh geometry={nodes.mesh_7.geometry} material={nodes.mesh_7.material} />
-      <mesh geometry={nodes.mesh_8.geometry} material={nodes.mesh_8.material} />
-      <mesh
-        geometry={nodes.Diamond_Round.geometry}
-        material={nodes.Diamond_Round.material}
-        position={[0, 0, 0.01]}
-        scale={[0.5, 0.5, 0.5]}
-      />
-    </group>
-  )
-}
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls component.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
+
 
 function Demo() {
     return (
     	<>
-	    	<Canvas>
+	    	<Canvas style={{margin: 'auto',height:1000,width:1000}}>
+	    		<CameraControls />
 	      		<Suspense fallback={null}>
-	        		<ArWing />
+	        		<Ring />
 	      		</Suspense>
 	      	</Canvas>
-	     </>
+		</>
     )
 }
 
