0c42dfb1b7e9554d7014dcd95b19d2884dad450c
Ryan Won <ryanwon7@gmail.com>
2021-06-17 22:52:13 -0400

    added files and basic page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from "./component/homepage.mp4"
+import Twitter from "./component/social/twitter.png"
+import Instagram from "./component/social/ig.png"
+import Facebook from "./component/social/fb.png"
+import Velverly from "./component/velverly.png"
+import Design from "./component/logo/design.png"
+import Share from "./component/logo/Share.png"
+import Surprise from "./component/logo/Surprise.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Velverly
-        </p>
-      </header>
+    <video
+      autoPlay
+      loop
+      muted
+      resizeMode="cover"
+      style={{
+        position: "absolute",
+        width: "100%",
+        left: "50%",
+        top: "50%",
+        height: "100%",
+        objectFit: "cover",
+        transform: "translate(-50%, -50%)",
+        zIndex: "-1"
+      }}
+    >
+      <source src={Homepage} type="video/mp4"/>
+    </video>
+    <div class="logo">
+      <div class="column_main">
+        <img src={Velverly} alt=""/>
+      </div>
+      <div class="column_sub">
+        <img src={Design} alt=""/>
+      </div>
+      <div class="column_sub">
+        <img src={Share} alt=""/>
+      </div>
+      <div class="column_sub">
+        <img src={Surprise} alt=""/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="column">
+        <img src={Instagram} class="twt" alt=""/>
+      </div>
+      <div class="column">
+        <img src={Facebook} class="twt" alt=""/>
+      </div>
+      <div class="column">
+        <img src={Twitter} class="twt" alt=""/>
+      </div>
+    </div>
+    
     </div>
   );
 }
+
 
 export default App;
